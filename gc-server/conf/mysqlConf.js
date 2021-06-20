199e121a63c2eb7dff37c286381cc29b8bfbcd71
Zizizi123 <38232815+Zizizi123@users.noreply.github.com>
2021-06-20 17:47:36 +0800

    Update mysqlConf.js

--- gc-server/conf/mysqlConf.js
+++ gc-server/conf/mysqlConf.js
@@ -2,8 +2,8 @@ module.exports = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: 'root',
-        database: 'gc',
+        password: '123456',
+        database: 'sys',
         // // 最大连接数，默认为10
         connectionLimit: 10
     }
